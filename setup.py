995f062c6b75c6e36cefc3a7169e57d733fd5cd0
Alfred Thoft <alfchr21@student.aau.dk>
2024-08-27 14:05:33 +0200

    works in sim

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+import os 
+from glob import glob  
+package_name = 'little_helper_commander'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='a',
+    maintainer_email='alfchr21@student.aau.dk',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'commander = little_helper_commander.little_helper_commander:main',
+            'item_tf_broadcaster = little_helper_commander.item_tf_broadcaster:main'
+        ],
+
+    },
+)
